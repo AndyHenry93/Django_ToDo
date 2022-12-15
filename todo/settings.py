235deb7de2512eab5a71d872270f6609642c5225
andy henry <a.henry250@gmail.com>
2022-12-15 14:35:36 -0500

    preparing for deployment

--- todo/settings.py
+++ todo/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-# CSRF_TRUSTED_ORIGINS = ['DOMAIN']
+ALLOWED_HOSTS = ['127.0.0.1','web-production-221a.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['web-production-221a.up.railway.app']
 
 
 # Application definition
