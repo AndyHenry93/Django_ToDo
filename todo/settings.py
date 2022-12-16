a59b8399a2eed0a58e007847afcda5be34c504f0
Andy Henry <A.henry250@gmail.com>
2022-12-15 21:59:02 -0500

    preparing for deployment

--- todo/settings.py
+++ todo/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','web-production-221a.up.railway.app']
 CSRF_TRUSTED_ORIGINS = ['https://web-production-221a.up.railway.app']
